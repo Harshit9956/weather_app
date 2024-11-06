1f2025e9f2acf26b26c6ff2fcf8c54c937a2ecf1
Harshit99565 <hm9956245171@gmail.com>
2024-11-06 21:34:27 +0530

    added home page icon

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask,render_template
+# import requests
+
+# city_name = 'Pune'
+# API_key='3bffd425426cabd79428a7cddc2eaa4f'
+# url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}&units=metric'
+
+# response= requests.get(url)
+# if response.status_code == 200:
+#     data = response.json()
+#     print(data)
+#     print(data['weather'][0]['description'])
+#     print('current temperature is',data['main']['temp'])
+#     print('current temperature Feels like  is ',data['main']['feels_like'])
+#     print('Humidity is',data['main']['humidity'])
+
+
+    
+
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/report')
+def report():
+    return render_template('report.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
